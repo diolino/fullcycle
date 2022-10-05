c73796308d5f436c760e1b71616c63fbe6e4c9bf
diolino <gtdiolino@gmail.com>
2022-10-05 18:27:30 -0300

    organizando diretorios

---
+++ desafio-nginx-com-nodejs/db/dump/dump.sql
@@ -0,0 +1,7 @@
+USE nodedb;
+
+CREATE TABLE IF NOT EXISTS people (
+  id int not null auto_increment,
+  name varchar(256),
+  primary key (id)
+);
--- desafio-nginx-com-nodejs/node/index.js
+++ desafio-nginx-com-nodejs/node/index.js
@@ -15,6 +15,7 @@ const mysql = require ('mysql')
 const sqlInsert = `INSERT INTO people(name) values (?)`
 const sqlSelect = `SELECT name FROM people`
 var pessoas = []
+var titulo = "<h1> Full Cycle !!!</h1>"
 var html = ""
 //connection.query(sqlInsert, 'Candidato do Povo')
 var htmlform = " <form action=\"/create\"> <label for=\"nome\"> nome </label> <input type=\"text\" id=\"nome\" name=\"nome\" value=\"\"> <br> <input type=\"submit\" value=\"Submit\"></form> "
@@ -22,15 +23,35 @@ var htmlform = " <form action=\"/create\"> <label for=\"nome\"> nome </label> <i
 //connection.end()
 
 app.get('/', (req, res) => {
+
+    var connection = mysql.createConnection(config)
+    var htmllist = "";
+
     msg = req.query.msg;
     msghtml = ""
     if(msg){
         msghtml = "<p>"+msg+"</p>";
     }
-    let htmllist = listCandidatos();
-    console.log("aqui no /: "+htmllist);
-    html = msghtml+"<h1>Cadastrar novo Candidato</h1>"+htmlform+"<h1>Lista de Candidatos: </h1>"+htmllist;
-    res.status(200).send(html);
+   
+    connection.query(sqlSelect, function (err, result, fields) {
+        if (err) throw err;
+        pessoas = result;       
+        htmllist="<ul>"
+       for (var i in pessoas) {
+        htmllist+="<li> Candidato "+i+": "+pessoas[i].name+"</li>"
+        //console.log('Candidato '+i+': '+pessoas[i].name)         
+       }
+       htmllist+="</ul>"
+       html = titulo+
+            msghtml+
+            "<h2>Cadastrar novo Candidato</h2>"+
+            htmlform+
+            "<h2>Lista de Candidatos: </h2>"
+            +htmllist;
+       res.status(200).send(html);       
+    });
+    
+    connection.end();   
 })
 
 app.get('/create', (req, res) => {
@@ -43,29 +64,7 @@ app.get('/create', (req, res) => {
     res.redirect("/?msg=Novo%20Candidato%20Inserido%20Com Sucesso");
 })
 
-
 app.listen (port, () => {
     console.log ('Rodando na porta' + port)
 })
-
-syncfunction listCandidatos(){
-    var connection = mysql.createConnection(config)
-    var htmllist = "";
-    connection.query(sqlSelect, function (err, result, fields) {
-        if (err) throw err;
-        pessoas = result;       
-        htmllist="<ul>"
-       for (var i in pessoas) {
-        htmllist+="<li> Candidato "+i+": "+pessoas[i].name+"</li>"
-        //console.log('Candidato '+i+': '+pessoas[i].name)         
-       }
-       htmllist+="</ul>"
-       console.log("aqui no listar: "+htmllist);       
-    });
-    connection.end();
-    console.log("aqui no listar 2: "+htmllist);
-    return htmllist;
-    //console.log('Temos '+pessoas.length+" candidatos") 
-    //console.log("iniciou")
-}
 
