3bd0600a5976253b3f8d94bfaf16a038c9861f16
diolino <gtdiolino@gmail.com>
2022-10-05 21:09:29 -0300

    criando desafio go

--- desafio-nginx-com-nodejs/node/index.js
+++ desafio-nginx-com-nodejs/node/index.js
@@ -15,10 +15,15 @@ const mysql = require ('mysql')
 const sqlInsert = `INSERT INTO people(name) values (?)`
 const sqlSelect = `SELECT name FROM people`
 var pessoas = []
-var titulo = "<h1> Full Cycle !!!</h1>"
+var titulo = "<h1>Full Cycle Rocks!</h1>"
 var html = ""
 //connection.query(sqlInsert, 'Candidato do Povo')
-var htmlform = " <form action=\"/create\"> <label for=\"nome\"> nome </label> <input type=\"text\" id=\"nome\" name=\"nome\" value=\"\"> <br> <input type=\"submit\" value=\"Submit\"></form> "
+var htmlform = 
+    " <form action=\"/create\">"+
+        " <label for=\"nome\"> Nome </label>"+
+        " <input type=\"text\" id=\"nome\" name=\"nome\" value=\"\"> "+
+        "<input type=\"submit\" value=\"Salvar\">"+
+    "</form> "
 //let htmllist = "";
 //connection.end()
 
@@ -38,15 +43,15 @@ app.get('/', (req, res) => {
         pessoas = result;       
         htmllist="<ul>"
        for (var i in pessoas) {
-        htmllist+="<li> Candidato "+i+": "+pessoas[i].name+"</li>"
+        htmllist+="<li> Pessoa "+i+": "+pessoas[i].name+"</li>"
         //console.log('Candidato '+i+': '+pessoas[i].name)         
        }
        htmllist+="</ul>"
        html = titulo+
             msghtml+
-            "<h2>Cadastrar novo Candidato</h2>"+
+            "<h2>Cadastrar nova Pessoa</h2>"+
             htmlform+
-            "<h2>Lista de Candidatos: </h2>"
+            "<h2>Lista de nomes cadastrada no banco de dados: </h2>"
             +htmllist;
        res.status(200).send(html);       
     });
@@ -61,7 +66,7 @@ app.get('/create', (req, res) => {
     connection.commit();    
     //listCandidatos();
     connection.end();
-    res.redirect("/?msg=Novo%20Candidato%20Inserido%20Com Sucesso");
+    res.redirect("/?msg=Nova%20pessoa%20inserida%20com sucesso");
 })
 
 app.listen (port, () => {
