683c3ed40a02f2083f4208a32fca13abea274214
diolino <gtdiolino@gmail.com>
2022-12-26 19:23:15 -0300

    desafio do handler de customer

---
+++ mod07-DDD-Pratica/node/src/domain/event/customer/handler/send_console_log2_when_customer_is_created.handler2.mensagem.ts
@@ -0,0 +1,10 @@
+import EventHandlerInterface from "../../@shared/event-handler.interface";
+import CustomerCreatedEvent from "../customer-created.event";
+
+export default class SendConsoleLog2WhenCustomerIsCreated
+  implements EventHandlerInterface<CustomerCreatedEvent>
+{
+  handle(event: CustomerCreatedEvent): void {
+    console.log(`Esse é o SEGUNDO console.log do evento: CustomerCreated`); //poderia enviar, por exemplo, para o RabbitMq
+  }
+}
---
+++ mod07-DDD-Pratica/node/src/domain/event/customer/handler/send_console_log_when_customer_address_is_changed.handler.mensagem.ts
@@ -0,0 +1,19 @@
+import Customer from "../../../entity/customer";
+import EventHandlerInterface from "../../@shared/event-handler.interface";
+import CustomerAddressChangedEvent from "../customer-adress_changed.event";
+import CustomerCreatedEvent from "../customer-created.event";
+
+export default class SendConsoleLogWhenCustomerAdressIsChanged
+  implements EventHandlerInterface<CustomerAddressChangedEvent>
+{
+  handle(event: CustomerAddressChangedEvent): void {
+    const customer = event.eventData as Customer;
+
+
+    //const nome = event.eventData.nome;
+    //const endereco = event.eventData.endereco;
+    //console.log(`Endereço do cliente: {id}, {nome} alterado para: {endereco}`); //poderia enviar, por exemplo, para o RabbitMq
+    console.log(`Endereço do cliente alterado: ${customer.id}, ${customer.name} alterado para 
+      ${customer.Address._street}, ${customer.Address._number}, ${customer.Address._city},${customer.Address._zip}`); //poderia enviar, por exemplo, para o RabbitMq
+  }
+}
